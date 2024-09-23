b4608992b9188329c62697a69d102c30b95100f5
cachoJDG <jgago@itba.edu.ar>
2024-09-23 20:10:50 -0300

    header fix with

---
+++ pages/api/mercadoPago.js
@@ -0,0 +1,23 @@
+// pages/api/mercadoPago.js
+import axios from 'axios';
+
+export default async function handler(req, res) {
+    if (req.method === 'POST') {
+        try {
+            const response = await axios.post('https://api.mercadopago.com/checkout/preferences', req.body, {
+                headers: {
+                    Authorization: `Bearer ${process.env.NEXT_PUBLIC_MP_ACCESS_TOKEN}`,
+                    'Content-Type': 'application/json'
+                }
+            });
+            res.status(200).json(response.data);
+        } catch (error) {
+            console.error('Error al crear la preferencia:', error.response ? error.response.data : error);
+            res.status(500).json({ error: 'Error al crear la preferencia' });
+        }
+    } else {
+        // Handle any other HTTP method
+        res.setHeader('Allow', ['POST']);
+        res.status(405).end(`Method ${req.method} Not Allowed`);
+    }
+}
